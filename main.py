6a06f193ab06d59be815df1c6bafa9be81d121c9
Wyatt Hoffman <wyatthoffman4392@gmail.com>
2022-03-18 07:18:31 -0700

    feat: comments-and-variables

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+
 def main():
-    print("Hola Mundo!")
+    # This is a comment, comments are used to explain code  and also make code easier to understand
+
+    # message and age are both variables, variables can be used as a location to store data
+    message = 'Hola Mundo! My name is John'  # message is of type str meaning string
+    age: 20 # age is of type int meaning integer
+    
+    print(message)
+    print(age)
 
 if __name__ == '__main__':
     main()
