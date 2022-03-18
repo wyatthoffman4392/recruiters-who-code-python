a77e1a54a04ab8e2c27321394ff46d397af8508d
Wyatt Hoffman <wyatthoffman4392@gmail.com>
2022-03-18 07:18:59 -0700

    feat: comments-and-variables

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
 
     # message and age are both variables, variables can be used as a location to store data
     message = 'Hola Mundo! My name is John'  # message is of type str meaning string
-    age: 20 # age is of type int meaning integer
+    age= 20 # age is of type int meaning integer
     
     print(message)
     print(age)
