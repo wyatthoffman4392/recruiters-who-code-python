1c8e1d5336a035aac7974116c1dfb7fb06fb0f82
Wyatt Hoffman <wyatthoffman4392@gmail.com>
2022-03-31 17:26:13 -0700

    feat:if-else

---
+++ if-else/if-else.py
@@ -0,0 +1,12 @@
+def main():
+    temp = 50
+
+    if temp > 70:
+        print('t-shirt')
+    elif temp < 70 and temp >= 50:
+        print('light jacket')
+    else:
+        print('heavy jacket')
+
+if __name__ == '__main__':
+    main()
