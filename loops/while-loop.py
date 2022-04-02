ccecac319a7b2d2ed0037614e7740ad9f3292f32
Wyatt Hoffman <wyatthoffman4392@gmail.com>
2022-04-02 06:55:32 -0700

    feat: while-loop

---
+++ loops/while-loop.py
@@ -0,0 +1,13 @@
+def main():
+    i = 1
+
+    # while loops only execute as long as a condition is true
+    while i < 25:
+        print(i)
+        i += 2
+    else:
+        print('That is all of the odd numbers 1 - 25')
+
+
+if __name__ == '__main__':
+    main()
