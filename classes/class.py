625c7b8596f458147a4416d7ba29c04d53c2b3bc
Wyatt Hoffman <wyatthoffman4392@gmail.com>
2022-07-06 08:06:22 -0700

    feat/car-class

---
+++ classes/class.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self, year, make, model):
+        self.year = year
+        self.make = make
+        self.model = model
+    
+    def get_car_info(self):
+        print(f'{self.year} {self.make} {self.model}')
+
+my_car = Car(1998, 'Honda', 'Civic')
+my_car.get_car_info()
